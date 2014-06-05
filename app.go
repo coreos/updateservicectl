05cb0760cf773fe659ca0fe7f993747ce2195f07
Cole Gleason <cole.gleason@coreos.com>
2014-06-04 17:41:27 -0700

    refactor(app): change app commands to use flags instead of positional args

--- app.go
+++ app.go
@@ -10,31 +10,48 @@ import (
 )
 
 var (
+	appFlags struct {
+		appId       StringFlag
+		label       StringFlag
+		description StringFlag
+	}
+
 	cmdListApps = &Command{
-		Name:  "list-apps",
-		Description: `List all of the apps that exist including their label,
-token and update state.`,
-		Run: listApps,
+		Name:        "list-apps",
+		Description: `List all of the apps that exist including their label, token and update state.`,
+		Run:         listApps,
 	}
 	cmdCreateApp = &Command{
 		Name:        "create-app",
-		Usage:       "[<appId>] <label> <description>",
-		Description: `Create a new application. If appId is not provided it will be created randomly.`,
+		Usage:       "[OPTION]...",
+		Description: `Create a new application.`,
 		Run:         createApp,
 	}
 	cmdUpdateApp = &Command{
 		Name:        "update-app",
-		Usage:       "<appId> <label> <description>",
-		Description: `Update an app given a label.`,
+		Usage:       "[OPTION]...",
+		Description: `Update an app's label or description.`,
 		Run:         updateApp,
 	}
 	cmdDeleteApp = &Command{
 		Name:        "delete-app",
-		Usage:       "<appId>",
-		Description: `Delete an app given an id.`,
+		Usage:       "[OPTION]...",
+		Description: `Delete an app.`,
 		Run:         deleteApp,
 	}
 )
+
+func init() {
+	cmdCreateApp.Flags.Var(&appFlags.appId, "app-id", "Application UUID. If not provided, one will be randomly generated.")
+	cmdCreateApp.Flags.Var(&appFlags.label, "label", "New application label.")
+	cmdCreateApp.Flags.Var(&appFlags.description, "description", "New application description.")
+
+	cmdUpdateApp.Flags.Var(&appFlags.appId, "app-id",  "Application ID to update.")
+	cmdUpdateApp.Flags.Var(&appFlags.label, "label", "Set application label.")
+	cmdUpdateApp.Flags.Var(&appFlags.description, "description", "Set application description.")
+
+	cmdDeleteApp.Flags.Var(&appFlags.appId, "app-id", "Application ID to delete.")
+}
 
 func formatApp(app *update.App) string {
 	return fmt.Sprintf("%s\t%s\t%s\n", app.Id, app.Label, app.Description)
@@ -58,29 +75,23 @@ func listApps(args []string, service *update.Service, out *tabwriter.Writer) int
 }
 
 func createApp(args []string, service *update.Service, out *tabwriter.Writer) int {
-	if !(len(args) == 2 || len(args) == 3) {
-		return ERROR_USAGE
+	if appFlags.appId.Get() == nil  {
+		appFlags.appId.Set(uuid.New())
 	}
-
-	if len(args) == 2 {
-		appId := uuid.New()
-		args = append([]string{appId}, args...)
-	}
-
-	updateAppHelper(args, service, out)
-	return OK
+	return updateAppHelper(service, out)
 }
 
 func updateApp(args []string, service *update.Service, out *tabwriter.Writer) int {
-	if len(args) != 3 {
+	if appFlags.appId.Get() == nil || appFlags.label.Get() == nil || appFlags.description.Get() == nil {
 		return ERROR_USAGE
 	}
-	return updateAppHelper(args, service, out)
+
+	return updateAppHelper(service, out)
 }
 
-func updateAppHelper(args []string, service *update.Service, out *tabwriter.Writer) int {
-	appReq := &update.AppUpdateReq{Label: args[1], Description: args[2]}
-	call := service.App.Update(args[0], appReq)
+func updateAppHelper(service *update.Service, out *tabwriter.Writer) int {
+	appReq := &update.AppUpdateReq{Label: appFlags.label.String(), Description: appFlags.description.String()}
+	call := service.App.Update(appFlags.appId.String(), appReq)
 	app, err := call.Do()
 
 	if err != nil {
@@ -94,11 +105,11 @@ func updateAppHelper(args []string, service *update.Service, out *tabwriter.Writ
 }
 
 func deleteApp(args []string, service *update.Service, out *tabwriter.Writer) int {
-	if len(args) != 1 {
+	if appFlags.appId.Get() == nil {
 		return ERROR_USAGE
 	}
 
-	call := service.App.Delete(args[0])
+	call := service.App.Delete(appFlags.appId.String())
 	app, err := call.Do()
 
 	if err != nil {
