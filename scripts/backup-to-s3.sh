97b5f49f1b8615b4f99027426b6fb5d930361694
Cole Gleason <cole.gleason@coreos.com>
2014-06-11 17:01:29 -0700

    chore(backup): add script to backup roller db to S3

---
+++ scripts/backup-to-s3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+tmpfile=`mktemp -t "roller-backup"`
+$dir/../bin/updatectl database backup $tmpfile
+
+bucket=roller-backups
+resource="/${bucket}/backup.tar.gz"
+contentType="application/x-compressed-tar"
+dateValue=`date -ju +"%a, %d %b %Y %H:%M:%S GMT"`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${S3_SECRET_KEY} -binary | base64`
+
+curl --upload-file ${tmpfile} \
+    -H "Date: ${dateValue}" \
+    -H "Content-type: ${contentType}" \
+    -H "Authorization: AWS ${S3_ACCESS_KEY}:${signature}" \
+    http://s3.amazonaws.com${resource}
