f5dd201763bf88424155aa80fda9dee2d0c0171c
Stephen Demos <stephen.demos@coreos.com>
2016-07-15 09:55:52 -0700

    instance: structure our machineid like a real machine id

--- instance.go
+++ instance.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strings"
 	"text/tabwriter"
 	"time"
 
@@ -336,7 +337,7 @@ func instanceFake(args []string, service *update.Service, out *tabwriter.Writer)
 
 	for i := 0; i < instanceFlags.clientsPerApp; i++ {
 		c := &Client{
-			Id:             uuid.New(),
+			Id:             strings.Replace(uuid.New(), "-", "", -1),
 			SessionId:      uuid.New(),
 			Version:        instanceFlags.version,
 			AppId:          instanceFlags.appId.String(),
